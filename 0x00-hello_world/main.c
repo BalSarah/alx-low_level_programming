3960e21c9c4b2f902522e94fdde6c0dca30ff049
BalSarah <mzsarahposh@gmail.com>
2023-09-07 09:52:14 -0700

    c commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -10,7 +10,7 @@ int main(void)
 	return (0);
 }
 julien@ubuntu:~/c/0x00$ export CFILE=main.c
-julien@ubuntu:~/c/0x00$ ./0-preprocessor 
+julien@ubuntu:~/c/0x00$ ./0-preprocessor
 julien@ubuntu:~/c/0x00$ tail c
 # 942 "/usr/include/stdio.h" 3 4
 
@@ -20,5 +20,5 @@ julien@ubuntu:~/c/0x00$ tail c
 # 3 "main.c"
 int main(void)
 {
- return (0);
+	return (0);
 }
