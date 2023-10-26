6d78572941ac0278a4cfb49883f6b5808729b57c
BalSarah <mzsarahposh@gmail.com>
2023-10-26 13:20:55 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,16 +2,18 @@
 
 /**
  * get_bit - it takes the value of a bit of index in a deci
- * @n: search for index or number 
+ * @n: search for index or number
  * @index: numbers of bit
  * Return: bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        int bit values;
+	int bit values;
 
-        if (index > 63)
-                return (-1);
-        bit values = (n >> index) & 1;
-        return (bit values);
+	if (index > 63)
+		return (-1);
+	bit values = (n >> index) & 1;
+
+	return (bit values);
+
 }
