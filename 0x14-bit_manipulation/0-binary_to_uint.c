ae7b4bc53a1583afdc7f82a6370f88902ed7ed76
BalSarah <mzsarahposh@gmail.com>
2023-10-26 12:57:26 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int alx;
-        unsigned int sarah = 0;
+	int alx;
+	unsigned int sarah = 0;
 
-        if (!b)
-                return (0);
-        for (alx = 0; b[alx]; alx++)
-        {
-                if (b[alx] < '0' || b[alx] > '1')
-                        return (0);
-                sarah = 2 * sarah + (b[alx] - '0');
-        }
-        return (sarah);
+	if (!b)
+		return (0);
+	for (alx = 0; b[alx]; alx++)
+	{
+		if (b[alx] < '0' || b[alx] > '1')
+			return (0);
+		sarah = 2 * sarah + (b[alx] - '0');
+	}
+	return (sarah);
 }
